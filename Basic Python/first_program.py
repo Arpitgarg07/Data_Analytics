a39184b191730cc099945327b4815e21e3519293
Arpitgarg07 <arpitgarg5689@gmail.com>
2026-06-02 16:07:08 +0530

    this is first commit

---
+++ Basic Python/first_program.py
@@ -0,0 +1,45 @@
+print(''' 
+        Arpit garg,
+        Kinshu Garg
+        Manju Garg''')
+
+
+########################
+a = 5
+b = 6
+
+print(a)
+print(b)
+
+temp = a
+a = b
+b = temp
+
+print(a)
+print(b)
+
+
+#######################
+
+
+x=12.5
+print(x)
+print(type(x))
+
+x = int(x)
+print(type(x))
+
+##########################################
+
+student_name = str(input('Enter your name: '))
+student_age = int(input('Enter your age: '))
+student_number = int(input('Enter your number: '))
+student_age = float(student_age)
+print("\n")
+print("Student Id Card Details")
+print("Name:",student_name)
+print("Age:",student_age)
+print("Phone Number:",student_number)
+print("\n")
+print(type(student_age))
+###################
